38c895edae74cd97effe32b3a742e7bc9e4e9d56
Dave Mateer <davemateer@gmail.com>
2015-04-20 07:14:18 +0100

    Added in a new test project - radio from nz

---
+++ Radiostr-SpotifyWebApi-master/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Radiostr.SpotifyWebApi;
+using Radiostr.SpotifyWebApi.Cache;
+using Radiostr.SpotifyWebApi.Http;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main() {
+            Console.WriteLine("hello");
+            Thing().Wait();
+            //Thing();
+            Console.ReadLine();
+        }
+
+        static async Task<string> Thing(){
+            var http = new RestHttpClient(new System.Net.Http.HttpClient());
+            var memCache = new RuntimeMemoryCache(System.Runtime.Caching.MemoryCache.Default);
+            var clientCredentialsAuthApi = new ClientCredentialsAuthorizationApi(http,System.Configuration.ConfigurationManager.AppSettings, memCache);
+            var api = new PlaylistsApi(http,clientCredentialsAuthApi);
+
+            var playlists =  await api.GetPlaylists("davemateer");
+            Trace.TraceInformation(playlists.ToString());
+            return "xx";
+        }
+    }
+}
