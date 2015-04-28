cf77405993b5408806b11fcd7af062822857c317
Dave Mateer <davemateer@gmail.com>
2015-04-28 06:42:46 +0100

    Wordpress automation selenium

---
+++ SeleniumTesting/WordPressAutomation/WordPressTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPressAutomation;
+
+namespace WordPressTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1(){
+            var c = new Class1();
+            c.Go();
+        }
+    }
+}
