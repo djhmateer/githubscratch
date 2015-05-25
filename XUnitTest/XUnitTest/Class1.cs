91278aa537cc81197955a4d617b5d26be80d8137
Dave Mateer <davemateer@gmail.com>
2015-05-25 07:19:07 +0100

    Xunit trying to get working

---
+++ XUnitTest/XUnitTest/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace XUnitTest
+{
+    public class Class1
+    {
+        [Fact]
+        public void Thing()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
