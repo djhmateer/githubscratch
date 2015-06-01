f43256bf3b85e57f822c00a4b94a523efc60d73f
Dave Mateer <davemateer@gmail.com>
2015-06-01 06:47:14 +0100

    Serilog working using XUnit2 as a runner

---
+++ SerilogTest/SerilogTest/StoreLogger.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+namespace SerilogTest
+{
+    public class StoreLogger
+    {
+        public void Save(string message)
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile(@"C:\Temp\Log-{Date}.txt")
+                .CreateLogger();
+            log.Information("Saving");
+        }
+    }
+}
---
+++ SerilogTest/SerilogTest/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace SerilogTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void Thing()
+        {
+            var storeLogger = new StoreLogger();
+            storeLogger.Save("test");
+            Assert.Equal(1, 1);
+        }
+    }
+}
