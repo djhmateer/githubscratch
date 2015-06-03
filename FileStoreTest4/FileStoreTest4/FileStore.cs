520c0c1007ca0e127e6c8ab49646037098aed5ef
Dave Mateer <davemateer@gmail.com>
2015-06-03 06:56:25 +0100

    adding a few files

---
+++ FileStoreTest4/FileStoreTest4/FileStore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace FileStoreTest4
+{
+    public class FileStore
+    {
+        // Strong indication that FileStore cannot work without a workingDirectory
+        // this is a pre-condition of the class
+        public FileStore(string workingDirectory)
+        {
+            // Fail fast.. so can't have an invalid state of no working directory
+            if (workingDirectory == null)
+                throw new ArgumentNullException("There must be a working Directory passed to save to!");
+            if (!Directory.Exists(workingDirectory))
+                throw new ArgumentException("The workingDirectory must exist", "workingDirectory");
+
+            this.WorkingDirectory = workingDirectory;
+        }
+
+        public string WorkingDirectory { get; private set; }
+
+        // A Command (returns void)
+        public void Save(int id, string message)
+        {
+            var path = this.GetFileName(id);
+            File.WriteAllText(path, message);
+        }
+
+        // A Query.. Never return null.  Agree with team that null is not a valid value to return
+        // Maybe<T> is good for dealing with a value that may not present
+        public Maybe<string> Read(int id)
+        {
+            var path = this.GetFileName(id);
+            if (!File.Exists(path))
+                return new Maybe<string>();
+
+            var message = File.ReadAllText(path);
+            // Never want message to be Null - that is what the previous step is for
+            return new Maybe<string>(message);
+        }
+
+        // A Query (so this shouldn't have any side effects)
+        public string GetFileName(int id)
+        {
+            // This can never be null as int is a value type, and workingDirectory is a pre-condition
+            return Path.Combine(this.WorkingDirectory, id + ".txt");
+        }
+    }
+}
---
+++ FileStoreTest4/FileStoreTest4/Maybe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FileStoreTest4
+{
+    public class Maybe<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> values;
+
+        public Maybe()
+        {
+            this.values = new T[0];
+        }
+
+        public Maybe(T value)
+        {
+            // Guard clause to stop a null being inputted
+            if (value == null)
+                throw new ArgumentNullException("Cannot be null - use return new Maybe<string>()");
+
+            this.values = new[] { value };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
