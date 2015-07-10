3bd88f47babc85da89d386595467a24c61506717
Dave Mateer <davemateer@gmail.com>
2015-07-10 07:01:25 +0100

    DaveStore decorator spike with diagram in visio

--- FileStoreTest4/FileStoreSpikes/Tests.cs
+++ FileStoreTest4/FileStoreSpikes/Tests.cs
@@ -1,37 +1,103 @@
 ﻿using System;
 using System.IO;
+using Serilog;
 using Xunit;
 
 namespace FileStoreSpikes
 {
-    public class Tests
+    public class ConsoleStart
     {
-        [Fact]
-        public void Thing()
+        public static void Main()
         {
-            Assert.Equal(1, 1);
-        }
-
-        [Fact]
-        public void DaveFileWriter_Save_GivenID2AndAMessage_ShouldSaveToA2TxtFile_InCurrentDiretory()
-        {
-            var daveFileWriter = new DaveFileWriter();
-            daveFileWriter.Save(2, "hello2");
-        }
-
-        [Fact]
-        public void DaveFileWriter_Save_ShouldLogSavingThenSavedToCurrentDirectory()
-        {
-            var daveFileWriter = new DaveFileWriter();
-            daveFileWriter.Save(2, "hello2");
+            Console.WriteLine("Hello world");
+            var cr = new CompositionRoot();
+            cr.Run();
+            Console.ReadLine();
         }
     }
 
-    public class DaveFileWriter
+    public class Tests
     {
-        public void Save(int id, string message){
+        //[Fact]
+        //public void DaveFileWriter_Save_GivenID2AndAMessage_ShouldSaveToA2TxtFile_InCurrentDiretory()
+        //{
+        //    var daveFileWriter = new DaveMessageStore();
+        //    daveFileWriter.Save(2, "hello2");
+        //}
+
+        //[Fact]
+        //public void DaveFileWriter_Save_ShouldLogSavingThenSavedToCurrentDirectory()
+        //{
+        //    var daveFileWriter = new DaveMessageStore();
+        //    daveFileWriter.Save(2, "hello2");
+        //}
+    }
+
+    public class CompositionRoot
+    {
+        DaveStore daveStore;
+
+        public CompositionRoot()
+        {
+            var daveWriter = new DaveWriter();
+            var daveLogger = new DaveLogger(daveWriter);
+            daveStore = new DaveStore(daveLogger);
+        }
+
+        public void Run()
+        {
+            // When save is called, want to call daveLogger.Save first
+            daveStore.Save(5, "Console Test");
+        }
+    }
+
+    public class DaveWriter : IDaveWriter
+    {
+        public void Save(int id, string message)
+        {
             var path = Environment.CurrentDirectory + @"\" + id + ".txt";
             File.WriteAllText(path, message);
         }
+    }
+
+    public class DaveLogger : IDaveWriter
+    {
+        private readonly IDaveWriter writer;
+        ILogger log;
+
+        public DaveLogger(IDaveWriter writer)
+        {
+            this.writer = writer;
+            log = new LoggerConfiguration()
+                .WriteTo.RollingFile(@"C:\Temp\Log-{Date}.txt")
+                .CreateLogger();
+        }
+
+        public void Save(int id, string message)
+        {
+            log.Information("Saving ", id);
+            this.writer.Save(id, message);
+            log.Information("Saved ", id);
+        }
+    }
+
+    public class DaveStore : IDaveWriter
+    {
+        private readonly IDaveWriter writer;
+
+        public DaveStore(IDaveWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void Save(int id, string message)
+        {
+            this.writer.Save(id, message);
+        }
+    }
+
+    public interface IDaveWriter
+    {
+        void Save(int id, string message);
     }
 }
